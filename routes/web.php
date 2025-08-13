c4a53dcd12b178d48e1b3713bf48be605aa9bf32
Rayane Tenorio <rayanemtenorio@gmail.com>
2025-08-13 02:07:38 -0300

    removendo rotas de login

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,16 @@ Route::get('/contato', function () { return view('site.home'); })->name('contato
 Route::get('/politica-de-privacidade', function () { return view('site.privacidade'); })->name('privacidade');
 Route::get('/termos-de-uso', function () { return view('site.termos'); })->name('termos');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+// Route::middleware(['auth'])->group(function () {
+//     Route::redirect('settings', 'settings/profile');
 
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});
+//     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+//     Volt::route('settings/password', 'settings.password')->name('settings.password');
+//     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+// });
 
 require __DIR__.'/auth.php';
