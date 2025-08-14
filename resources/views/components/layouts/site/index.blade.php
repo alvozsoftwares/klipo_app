e0beb6bb17a906b57c0e86722bb17c582e39415e
Rayane Tenorio <rayanemtenorio@gmail.com>
2025-08-14 00:52:48 -0300

    tag manager

--- resources/views/components/layouts/site/index.blade.php
+++ resources/views/components/layouts/site/index.blade.php
@@ -32,6 +32,14 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KMZ7Z9SW');</script>
+    <!-- End Google Tag Manager -->
+
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-L7KFRPW28R"></script>
     <script>
@@ -47,6 +55,11 @@
     @endif
 </head>
 <body class="min-h-screen antialiased gradiente flex flex-col relative">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMZ7Z9SW"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <div x-data="{ loading: true }" x-init="setTimeout(() => loading = false, 500)">
 
         <!-- Splash screen -->
